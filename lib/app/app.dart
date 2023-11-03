e0a8609902126e979409eecb036f4152ee531982
Hatem Kobicy <hatem.kobicy@gmail.com>
2023-11-03 21:46:59 +0100

    first commit

---
+++ lib/app/app.dart
@@ -0,0 +1,47 @@
+// ignore_for_file: must_be_immutable
+
+import 'package:advanced_flutter_arabic/app/app_prefs.dart';
+import 'package:advanced_flutter_arabic/presentation/resources/theme_manager.dart';
+import 'package:easy_localization/easy_localization.dart';
+import 'package:flutter/material.dart';
+
+import '../presentation/resources/routes_manager.dart';
+import 'di.dart';
+
+class MyApp extends StatefulWidget {
+  // named constructor
+  MyApp._internal();
+
+  int appState = 0;
+
+  static final MyApp _instance =
+      MyApp._internal(); // singleton or single instance
+
+  factory MyApp() => _instance; // factory
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final AppPreferences _appPreferences = instance<AppPreferences>();
+
+  @override
+  void didChangeDependencies() {
+    _appPreferences.getLocal().then((local) => {context.setLocale(local)});
+    super.didChangeDependencies();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      localizationsDelegates: context.localizationDelegates,
+      supportedLocales: context.supportedLocales,
+      locale: context.locale,
+      debugShowCheckedModeBanner: false,
+      onGenerateRoute: RouteGenerator.getRoute,
+      initialRoute: Routes.splashRoute,
+      theme: getApplicationTheme(),
+    );
+  }
+}
